4c56ced4077efc88f96b8070fba45d70020633b9
bilalbarina <bilalbarina1@gmail.com>
2020-08-13 20:09:41 +0100

    Update init.php

--- init.php
+++ init.php
@@ -1,5 +1,5 @@
 <?php
 
 $targetCountry = "AU";
-$myLink = "https://86400.au";
+$myLink = "https://smrturl.co/o/57870/53165098?s1=";
 $redLink = "https://play.google.com/store/apps/details?id=au.com.bank86400&hl=en_US";
