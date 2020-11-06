152760b0c93be730a9798b5848c2b9bd40666492
bilalbarina <bilalbarina1@gmail.com>
2020-11-07 00:37:28 +0800

    Update init.php

--- init.php
+++ init.php
@@ -1,5 +1,5 @@
 <?php
 
 $targetCountry = "ES";
-$myLink = "https://www.amazon.es/gp/product/B015FSC5RM/ref=as_li_tl?ie=UTF8&camp=3638&creative=24630&creativeASIN=B015FSC5RM&linkCode=as2&tag=bb4rina-21&linkId=a2e67fe8ac8cc40f0d32d971bad108b1";
-$redLink = "https://www.amazon.es/dp/B015FSC5RM";
+$myLink = "https://www.amazon.es/gp/product/B07CDSJS74/ref=as_li_tl?ie=UTF8&camp=3638&creative=24630&creativeASIN=B07CDSJS74&linkCode=as2&tag=bb4rina-21&linkId=5c97541f2fcdb5f493634b3f3d23502a";
+$redLink = "https://www.amazon.es/gp/product/B07CDSJS74";
