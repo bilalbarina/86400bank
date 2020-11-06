092fe1a9300d6ad99ff0b8300f96ebdfcef7fbbe
bilalbarina <bilalbarina1@gmail.com>
2020-11-07 03:02:01 +0800

    Update init.php

--- init.php
+++ init.php
@@ -1,5 +1,5 @@
 <?php
 
 $targetCountry = "ES";
-$myLink = "https://www.amazon.es/gp/product/B082PJJPVN/ref=as_li_tl?ie=UTF8&camp=3638&creative=24630&creativeASIN=B082PJJPVN&linkCode=as2&tag=bb4rina-21&linkId=b81147dd6122bfec7d7cb20b14a56fab";
-$redLink = "https://www.amazon.es/Beyblade-Spryzen-Roktavor-Hasbro-E7729ES0/dp/B082PJJPVN";
+$myLink = "http://gbe.st/302of39";
+$redLink = "https://www.gearbest.com/bluetooth-headphones/pp_009726597661.html?wid=1349303";
