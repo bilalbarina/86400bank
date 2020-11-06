040641a686abc6a1837ddeeb145a5d642a49c4c9
bilalbarina <bilalbarina1@gmail.com>
2020-11-07 00:05:16 +0800

    Update init.php

--- init.php
+++ init.php
@@ -1,5 +1,5 @@
 <?php
 
-$targetCountry = "AU";
-$myLink = "https://smrturl.co/o/57870/53165098?s1=";
-$redLink = "https://www.86400.com.au/";
+$targetCountry = "ES";
+$myLink = "https://www.amazon.es/gp/product/B005GI4NJA/ref=as_li_tl?ie=UTF8&camp=3638&creative=24630&creativeASIN=B005GI4NJA&linkCode=as2&tag=bb4rina-21&linkId=f008241c758d178d1967a788ea2b4b8a";
+$redLink = "https://www.amazon.es/gp/product/B005GI4NJA?psc=1";
