2d26823d0b06e1ac039278465d43ef7b79564c57
bilalbarina <bilalbarina1@gmail.com>
2020-11-07 00:16:32 +0800

    Update init.php

--- init.php
+++ init.php
@@ -1,5 +1,5 @@
 <?php
 
 $targetCountry = "ES";
-$myLink = "https://www.amazon.es/gp/product/B005GI4NJA/ref=as_li_tl?ie=UTF8&camp=3638&creative=24630&creativeASIN=B005GI4NJA&linkCode=as2&tag=bb4rina-21&linkId=f008241c758d178d1967a788ea2b4b8a";
-$redLink = "https://www.amazon.es/gp/product/B005GI4NJA?psc=1";
+$myLink = "https://www.amazon.es/gp/product/B005GI4NJA";
+$redLink = "https://l.linklyhq.com/l/9S2N";
