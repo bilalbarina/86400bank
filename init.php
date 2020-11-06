8e403e1f9ba6f4235fa3af4336291f75b6668d55
bilalbarina <bilalbarina1@gmail.com>
2020-11-07 01:41:06 +0800

    Update init.php

--- init.php
+++ init.php
@@ -1,5 +1,5 @@
 <?php
 
 $targetCountry = "ES";
-$myLink = "https://www.amazon.es/gp/product/B07CDSJS74/ref=as_li_tl?ie=UTF8&camp=3638&creative=24630&creativeASIN=B07CDSJS74&linkCode=as2&tag=bb4rina-21&linkId=5c97541f2fcdb5f493634b3f3d23502a";
-$redLink = "https://www.amazon.es/gp/product/B07CDSJS74";
+$myLink = "https://www.amazon.es/gp/product/B01J3BXH7E/ref=as_li_tl?ie=UTF8&camp=3638&creative=24630&creativeASIN=B01J3BXH7E&linkCode=as2&tag=bb4rina-21&linkId=3b1b1cb77292273ac334a392e266aa17";
+$redLink = "https://www.amazon.es/Garant%C3%ADa-Avantree-Ajustable-Ordenador-Escritorio/dp/B01J3BXH7E?ref_=Oct_DLandingS_D_c2b30599_66&smid=A2JYKDL02CLV4Y";
