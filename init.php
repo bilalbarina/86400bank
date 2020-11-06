b3e4c6cd80e88fa63959cb93bca0755e8a8ec962
bilalbarina <bilalbarina1@gmail.com>
2020-11-07 02:09:33 +0800

    Update init.php

--- init.php
+++ init.php
@@ -1,5 +1,5 @@
 <?php
 
 $targetCountry = "ES";
-$myLink = "https://www.amazon.es/gp/product/B01J3BXH7E/ref=as_li_tl?ie=UTF8&camp=3638&creative=24630&creativeASIN=B01J3BXH7E&linkCode=as2&tag=bb4rina-21&linkId=3b1b1cb77292273ac334a392e266aa17";
-$redLink = "https://www.amazon.es/Garant%C3%ADa-Avantree-Ajustable-Ordenador-Escritorio/dp/B01J3BXH7E?ref_=Oct_DLandingS_D_c2b30599_66&smid=A2JYKDL02CLV4Y";
+$myLink = "https://www.amazon.es/gp/product/B082PJJPVN/ref=as_li_tl?ie=UTF8&camp=3638&creative=24630&creativeASIN=B082PJJPVN&linkCode=as2&tag=bb4rina-21&linkId=b81147dd6122bfec7d7cb20b14a56fab";
+$redLink = "https://www.amazon.es/Beyblade-Spryzen-Roktavor-Hasbro-E7729ES0/dp/B082PJJPVN";
